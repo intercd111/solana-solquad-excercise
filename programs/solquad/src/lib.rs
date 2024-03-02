ec8307688e75751d1b4f92040d1b4d18c95279fe
intercd111 <161942056+intercd111@users.noreply.github.com>
2024-03-02 19:33:32 +0700

    Update lib.rs

--- programs/solquad/src/lib.rs
+++ programs/solquad/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5sFUqUTjAMJARrEafMX8f4J1LagdUQ9Y8TR8HwGNHkU8");
+declare_id!("FJGURyAq54sBahBMnC1LesBTKxKQFEttf7vs9zPZoiTg");
 
 #[program]
 pub mod solquad {
